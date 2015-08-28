cb62447794d2672500284f83a3efa7a232dd04f3
j1lin0101 <jelin1@697-334-Erech.sf.k12.internal>
2015-08-28 09:57:16 -0700

    Final Changes

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,17 +1,44 @@
-PImage img
-
 void setup()
 {
-	size(512, 512);
-	
+  size(512, 512);
+
 }
 void draw() {
-  triangle(256, 50, 156, 200, 356, 200);
+  stroke(50);
+  outline();
+  
 }
 
+void outline(){
+    fill(255,255, 0);
+    triangle(256, 50, 156, 200, 356, 200);
+}
 
-void mousePressed() {
-img = loadImage("illuminati.jpg");
-if (mousePressed && (mouseButton == LEFT)) {
-    texture(img);
-  }
+void mousePressed(){
+    outline();
+    eye();
+    pupil();
+    browz();
+    textAlign(CENTER, BOTTOM);
+    fill(0, 0,0);
+    text("ILLUMINATI CONFIRMED", 256, 230);
+}
+
+void eye(){
+  fill(255, 250, 250);
+  arc(256, 130, 95, 45, PI, 2*PI);
+  arc(256, 130, 95, 45, 0, PI);
+ 
+}
+
+void pupil(){
+  stroke(7);
+  fill(0);
+  ellipse(256, 130, 12, 45);
+}
+
+void browz(){
+    stroke(20);
+    fill(255, 255, 0);
+    arc(256, 100, 65, 12, PI, 2*PI);
+}
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -14,28 +14,50 @@ import java.io.IOException;
 
 public class OriginalDesign extends PApplet {
 
-
-
 public void setup()
 {
-	size(512, 512);
-	
-}
-public void draw(){
-outline();
-}
+  size(512, 512);
 
-public void mousePressed() {
-PImage img=loadImage("illuminati.jpg");
-beginShape();
-texture(img);
-triangle(256, 50, 156, 200, 356, 200);
-endShape();
+}
+public void draw() {
+  stroke(50);
+  outline();
+  
 }
 
 public void outline(){
-triangle(256, 50, 156, 200, 356, 200);
- }
+    fill(255,255, 0);
+    triangle(256, 50, 156, 200, 356, 200);
+}
+
+public void mousePressed(){
+    outline();
+    eye();
+    pupil();
+    browz();
+    textAlign(CENTER, BOTTOM);
+    fill(0, 0,0);
+    text("ILLUMINATI CONFIRMED", 256, 230);
+}
+
+public void eye(){
+  fill(255, 250, 250);
+  arc(256, 130, 95, 45, PI, 2*PI);
+  arc(256, 130, 95, 45, 0, PI);
+ 
+}
+
+public void pupil(){
+  stroke(7);
+  fill(0);
+  ellipse(256, 130, 12, 45);
+}
+
+public void browz(){
+    stroke(20);
+    fill(255, 255, 0);
+    arc(256, 100, 65, 12, PI, 2*PI);
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
     if (passedArgs != null) {
